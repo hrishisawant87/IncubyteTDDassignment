882bcf020ca66fa41bb5196fbd69d0ceef5955dc
Hrishikesh Sawant <hrishi.sawant87@gmail.com>
2024-12-17 22:54:37 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static int add(String numbers){
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        String numbers="1,2";
+        add(numbers);
+
+        System.out.println("Hello world!");
+    }
+}
